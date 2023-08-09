0eb4d602baa11a90d28235aaf2b67f21c654ef30
Anup Kumar <30943490+anup-wa@users.noreply.github.com>
2023-08-09 18:28:00 +0530

    Update

--- fileManager/aws-config.js
+++ fileManager/aws-config.js
@@ -3,8 +3,8 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({
   region: 'ap-south-1',
-  accessKeyId: 'AKIAW6JPEX7SD6MPB46V',
-  secretAccessKey: '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'
+  accessKeyId: 'NULL',
+  secretAccessKey: 'Null'
 });
 
 const s3 = new AWS.S3();
